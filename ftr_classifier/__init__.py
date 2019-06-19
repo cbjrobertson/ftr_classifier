d901e474d6ea150fe38cec8e1077e4884afd9cd2
Cole Robertson <cbjrobertson@gmail.com>
2019-06-19 23:36:14 +0100

    updated imports, updated version

--- ftr_classifier/__init__.py
+++ ftr_classifier/__init__.py
@@ -6,13 +6,27 @@ Created on Fri May 31 22:32:52 2019
 @author: cole roberson
 """
 #imports
-import ftr_classifier.classify
-import ftr_classifier.count_lemmas
-import ftr_classifier.word_lists
-from ftr_classifier.classify import prepare,score,apply_dominance,classify_df,clean_spacy
-from ftr_classifier.word_lists import FEATURES, WORD_LISTS,ALL_FEATURES,MAIN_FEATURES,DOMINANT_FEATURES,\
-                        SUBMISSIVE_FEATURES,DOMINATED_FEATURES,SUMMARY_FEATURES,EXTRA_FEATURES
+import ftr_classifier.classify as classify
+import ftr_classifier.count_lemmas as cl
+import ftr_classifier.word_lists as word_lists
 from ftr_classifier.count_lemmas import count_lemmas
+from ftr_classifier.classify import prepare
+from ftr_classifier.classify import score
+from ftr_classifier.classify import apply_dominance
+from ftr_classifier.classify import classify_df
+from ftr_classifier.classify import clean_spacy
+from ftr_classifier.classify import _debug_check_words
+from ftr_classifier.word_lists import FEATURES
+from ftr_classifier.word_lists import WORD_LISTS
+from ftr_classifier.word_lists import ALL_FEATURES
+from ftr_classifier.word_lists import MAIN_FEATURES
+from ftr_classifier.word_lists import DOMINANT_FEATURES
+from ftr_classifier.word_lists import SUBMISSIVE_FEATURES
+from ftr_classifier.word_lists import DOMINATED_FEATURES
+from ftr_classifier.word_lists import SUMMARY_FEATURES,
+from ftr_classifier.word_lists import EXTRA_FEATURES
+
+#add versinos
 from ftr_classifier.info import INFO
 __version__ = INFO['version']
 description = INFO['description']
